81045af5f56bb6a310a27a6a6fd558dd391b802c
viet34tqc <viet34tqc@gmail.com>
2025-04-14 23:46:00 +0700

    Update schema

--- src/app/posts/_constants.ts
+++ src/app/posts/_constants.ts
@@ -5,24 +5,24 @@ export const MOCK_POSTS = [
     id: '1',
     title: 'post 1',
     content: 'This is the content of post 1',
-    status: 'published' as const,
+    status: 'PUBLISHED' as const,
   },
   {
     id: '2',
     title: 'post 2',
     content: 'This is the content of post 2',
-    status: 'draft' as const,
+    status: 'DRAFT' as const,
   },
   {
     id: '3',
     title: 'post 3',
     content: 'This is the content of post 3',
-    status: 'pending' as const,
+    status: 'PENDING' as const,
   },
 ]
 
 export const POST_STATUS_ICONS = {
-  draft: Pencil,
-  published: Check,
-  pending: Hourglass,
+  DRAFT: Pencil,
+  PUBLISHED: Check,
+  PENDING: Hourglass,
 }
