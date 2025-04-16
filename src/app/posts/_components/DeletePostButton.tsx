4b97a8872678bafeb194d12571a6dd006cfb5ad4
viet34tqc <viet34tqc@gmail.com>
2025-04-16 22:45:00 +0700

    Replace window location with redirect

--- src/app/posts/_components/DeletePostButton.tsx
+++ src/app/posts/_components/DeletePostButton.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
+import { redirect } from 'next/navigation'
 import { deletePost } from '../_actions/deletePost'
 
 export const DeletePostButton = ({ id }: { id: string }) => {
   const handleDelete = async () => {
     await deletePost(id)
-    window.location.href = '/posts'
+    redirect('/posts')
   }
 
   return <Button onClick={handleDelete}>Delete</Button>
