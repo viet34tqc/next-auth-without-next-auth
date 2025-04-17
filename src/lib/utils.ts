72d380a22dbb18383f0486ade07211f3027a81a0
viet34tqc <viet34tqc@gmail.com>
2025-04-17 18:38:52 +0700

    Fix build

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,55 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { ZodError } from 'zod'
+
+export type FormState = {
+  status: 'UNSET' | 'SUCCESS' | 'ERROR'
+  message: string
+  fieldErrors: Record<string, string[] | undefined>
+  timestamp: number
+}
+
+export const EMPTY_FORM_STATE: FormState = {
+  status: 'UNSET' as const,
+  message: '',
+  fieldErrors: {},
+  timestamp: Date.now(),
+}
+
+export const fromErrorfromMessageToFormState = (error: unknown) => {
+  // if validation error with Zod, return first error message
+  if (error instanceof ZodError) {
+    return {
+      status: 'ERROR' as const,
+      message: '',
+      fieldErrors: error.flatten().fieldErrors,
+      timestamp: Date.now(),
+    }
+  } else if (error instanceof Error) {
+    return {
+      status: 'ERROR' as const,
+      message: error.message,
+      fieldErrors: {},
+      timestamp: Date.now(),
+    }
+  } else {
+    return {
+      status: 'ERROR' as const,
+      message: 'An unknown error occurred',
+      fieldErrors: {},
+      timestamp: Date.now(),
+    }
+  }
+}
+
+export const fromMessageToFormState = (status: FormState['status'], message: string): FormState => {
+  return {
+    status,
+    message,
+    fieldErrors: {},
+    timestamp: Date.now(),
+  }
+}
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
