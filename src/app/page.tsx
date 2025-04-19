8795c72372e00b3f0a2cadf38ad9dd0093cddd58
viet34tqc <viet34tqc@gmail.com>
2025-04-20 00:10:28 +0700

    Update readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,12 @@ import PageHeader from '@/components/layout/PageHeader'
 import { PATHS } from '@/path'
 import Link from 'next/link'
 
-// src/app/page.tsx
 const HomePage = () => {
   return (
     <div className='space-y-4'>
       <PageHeader title='Home' />
       <Link href={PATHS.posts()} className='underline'>
-        Tickets
+        Posts
       </Link>
     </div>
   )
--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { Separator } from '../ui/separator'
 
 type Props = {
